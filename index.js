9662e08a8fa553aa05f39f2c6d6156122e9c4b56
Rodrigo Delgado <rdalcega@gmail.com>
2016-05-14 16:01:38 +0200

    removed all elements of index.js that were abstruse

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var app = express();
-
+// port is not one of the application settings properties
+// nonetheless, setting port to one of the values in the
+// disjunction allows us to refer to that value using
+// app.get('port') from here on
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+// app.use(express.static(__dirname + '/public'));
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+app.get('/', function(request, response) {
+  response.send('this is the website for miamifruit');
 });
 
 
